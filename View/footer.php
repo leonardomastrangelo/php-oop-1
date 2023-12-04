35fd7ca172494a22e6c90d0550c7835fa67d822e
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-12-04 14:51:50 +0100

    raw structure

---
+++ View/footer.php
@@ -0,0 +1,8 @@
+<footer class="text-center">
+    <span class="uppercase fs-5">
+        made with &hearts; and stress
+    </span>
+</footer>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include __DIR__ . "/View/header.php";
+?>
+<main class="container">
+    main
+</main>
+<?php
+include __DIR__ . "/View/footer.php";
+?>
